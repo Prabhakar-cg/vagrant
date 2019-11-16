37d6cd59571d89a443d8fc15197f421ca5a7227a
Prabhakar <narayn.prabhakar@gmail.com>
2019-11-16 15:12:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+module "ec2_cluster" {
+  source                 = "https://github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+  version                = "~> 2.0"
+  name                   = "my-cluster"
+  instance_count         = 5
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["abcd123"]
+  subnet_id              = "abcd1234"
 
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
