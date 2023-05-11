6d4677ef31595d062b685de20e79d9dccb2c8d17
Prabha <narayn.prabhakar@gmail.com>
2023-05-11 23:32:05 +0100

    groovy

--- folder.groovy
+++ folder.groovy
@@ -2,3 +2,15 @@ folder('artemis') {
     displayName('Artemis')
     description('Artemis mission')
 }
+folder('artemis/newmoon') {
+    displayName('New Moon')
+}
+folder('artemis/waxingcrescent') {
+    displayName('Waxing Crescent')
+}
+folder('artemis/firstquarter') {
+    displayName('First Quarter')
+}
+folder('artemis/waxinggibbous') {
+    displayName('Waxing Gibbous')
+}
