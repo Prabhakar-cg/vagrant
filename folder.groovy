2f1dd16ebc387e3c568f3b1abef9ae772650d29a
Prabha <narayn.prabhakar@gmail.com>
2024-05-11 16:31:44 +0100

    Update folder.groovy

--- folder.groovy
+++ folder.groovy
@@ -1,26 +1,3 @@
 folder('artemis') {
     displayName('Artemis')
     description('Artemis mission')
-}
-folder('artemis/newmoon') {
-    displayName('New Moon')
-}
-folder('artemis/waxingcrescent') {
-    displayName('Waxing Crescent')
-}
-folder('artemis/firstquarter') {
-    displayName('First Quarter')
-}
-folder('artemis/waxinggibbous') {
-    displayName('Waxing Gibbous')
-}
-
-folder('Artemis') {
-    authorization {
-        permission('hudson.model.Item.Create:authenticated')
-        permission('voyager', [
-                'hudson.model.Item.Create',
-                'hudson.model.Item.Discover'
-                ])
-    }
-}
