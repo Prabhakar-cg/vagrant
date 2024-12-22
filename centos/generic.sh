97c988f3c7826c79e1b44928c99c40c8a22f3684
Prabha <p2p91@exist.com>
2024-12-22 15:34:52 +0000

    2 new moons

--- centos/generic.sh
+++ centos/generic.sh
@@ -2,8 +2,8 @@
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl reload sshd
 #yum install ansible -y
-# yum -y upgrade
-# yum -y groupinstall "Development Tools"
+yum -y upgrade
+yum -y groupinstall "Development Tools"
 # sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 # sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 # sudo yum install fontconfig java-17-openjdk -y
